c896aa92770801d54950ffb6f76adef3144fce5a
Christian Dantas Germano <51031714+ChristianDantasGermano@users.noreply.github.com>
2020-11-17 20:36:25 -0300

    Add files via upload

---
+++ Nutricao/src/main/java/br/com/nutricao/Nutricao/NutricaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutricao.Nutricao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricaoApplication.class, args);
+	}
+
+}
